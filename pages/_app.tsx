4869fc960fcb2271d869f56b4c12bc4b4bd8957c
Miller Wesselhoff <millerwesselhoff@gmail.com>
2025-03-28 14:40:15 -0500

    change squiggle to waves, full size appshell

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import "@mantine/core/styles.css";
 import Head from "next/head";
 import { AppShell, Box, MantineProvider } from "@mantine/core";
 import { theme } from "../theme";
-import { SquiggleBackground } from "../components/SquiggleBackground";
+import { WaveBackground } from "../components/WaveBackground";
 
 export default function App({ Component, pageProps }: any) {
   return (
@@ -16,8 +16,8 @@ export default function App({ Component, pageProps }: any) {
         <link rel="shortcut icon" href="/favicon.svg" />
       </Head>
       <Box style={{ position: "relative", minHeight: "100vh" }}>
-        <AppShell>
-          <SquiggleBackground />
+        <AppShell style={{minHeight: "100vh"}}>
+          <WaveBackground />
           <Component {...pageProps} />
         </AppShell>
       </Box>
